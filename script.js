37fa4a0781d3f9e7ad018492a21ff426de5deed1
Samia Ferdous <samia.ferdous@icloud.com>
2021-03-21 22:46:42 -0400

    display questions dynamically

--- script.js
+++ script.js
@@ -44,3 +44,56 @@ var questions = [
         answer: "&",
     }
 ]
+
+// Display Questions one after the other
+function displayQuestions() {
+    var holdQ1Title = questions[i].title
+    hElement.textContent = holdQ1Title
+    var holdq1Choice1 = questions[i].choices[0];
+    var holdq1Choice2 = questions[i].choices[1];
+    var holdq1Choice3 = questions[i].choices[2];
+    var holdq1Choice4 = questions[i].choices[3];
+
+    oderListEl.innerHTML = '';
+
+    var liTag1 = document.createElement("li");
+    liTag1.setAttribute("class", "all_li")
+    var btn = document.createElement('button');
+    btn.setAttribute("class", "all_btn")
+    btn.textContent = holdq1Choice1;
+    liTag1.appendChild(btn)
+    oderListEl.appendChild(liTag1);
+    divContEL.appendChild(oderListEl);
+
+    var liTag2 = document.createElement("li");
+    liTag2.setAttribute("class", "all_li");
+    var btn2 = document.createElement('button');
+    btn2.setAttribute("class", "all_btn")
+    btn2.textContent = holdq1Choice2;
+    liTag2.appendChild(btn2)
+    oderListEl.appendChild(liTag2)
+    divContEL.appendChild(oderListEl);
+
+    var liTag3 = document.createElement("li");
+    liTag3.setAttribute("class", "all_li")
+    var btn3 = document.createElement('button');
+    btn3.setAttribute("class", "all_btn")
+    btn3.textContent = holdq1Choice3;
+    liTag3.appendChild(btn3)
+    oderListEl.appendChild(liTag3)
+    divContEL.appendChild(oderListEl);
+
+    var liTag4 = document.createElement("li");
+    liTag4.setAttribute("class", "all_li")
+    var btn4 = document.createElement('button');
+    btn4.setAttribute("class", "all_btn");
+    btn4.textContent = holdq1Choice4;
+    liTag4.appendChild(btn4);
+    oderListEl.appendChild(liTag4);
+    divContEL.appendChild(oderListEl);
+    var allBtnEl = document.querySelectorAll(".all_btn")
+    allBtnEl.forEach(function (event) {
+        event.addEventListener("click", onclickHandler)
+    });
+}
+
