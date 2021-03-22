d5347a9ed52255e35e983c08373a23c724717f0e
Samia Ferdous <samia.ferdous@icloud.com>
2021-03-21 22:14:14 -0400

    new variables

---
+++ script.js
@@ -0,0 +1,17 @@
+//Creating Variables
+
+var timeElement = document.querySelector("#time");
+var wrapperElement = document.querySelector(".start-quiz");
+var btnElement = document.querySelector("#start");
+var divContEL = document.querySelector(".questions");
+var hElement = document.querySelector("#title");
+var oderListEl = document.querySelector("#list-questions");
+var finishDiv = document.querySelector(".the-end");
+var finalScore = document.querySelector("#result");
+var errMsg = document.querySelector("#errorSmg");
+var initialInput = document.querySelector("#inputInitial").value;
+var submitEl = document.querySelector(".btn btn-primary mb-2");
+var responsDiv = document.querySelector("#response");
+var finaPageEl = document.querySelector(".final-page");
+var initialAndScore = document.querySelector("#staticEmail");
+var firstPageEl = document.querySelector(".intro");
